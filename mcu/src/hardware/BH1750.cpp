94fc63eca98af69ae3b5d9e357b9f769fe828134
Kasumi <therealkasumi@gmx.de>
2022-12-27 14:23:32 +0100

    Add license

--- mcu/src/hardware/BH1750.cpp
+++ mcu/src/hardware/BH1750.cpp
@@ -3,7 +3,20 @@
  * @author TheRealKasumi
  * @brief Implementation of the {@link TL::BH1750}.
  *
- * @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2022 TheRealKasumi
+ * 
+ * This project, including hardware and software, is provided "as is". There is no warranty
+ * of any kind, express or implied, including but not limited to the warranties of fitness
+ * for a particular purpose and noninfringement. TheRealKasumi (https://github.com/TheRealKasumi)
+ * is holding ownership of this project. You are free to use, modify, distribute and contribute
+ * to this project for private, non-commercial purposes. It is granted to include this hardware
+ * and software into private, non-commercial projects. However, the source code of any project,
+ * software and hardware that is including this project must be public and free to use for private
+ * persons. Any commercial use is hereby strictly prohibited without agreement from the owner.
+ * By contributing to the project, you agree that the ownership of your work is transferred to
+ * the project owner and that you lose any claim to your contribute work. This copyright and
+ * license note applies to all files of this project and must not be removed without agreement
+ * from the owner.
  *
  */
 #include "hardware/BH1750.h"
