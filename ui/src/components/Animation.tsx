a9afd575541bc7ee823b52d08ccc9dae57b8ddd6
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2023-01-14 16:49:42 +0100

    feat(ui): add new animations (#94)
    Closes: #92

--- ui/src/components/Animation.tsx
+++ ui/src/components/Animation.tsx
@@ -1,21 +1,57 @@
-export enum Animation {
+enum RainbowMode {
+  Solid,
+  Linear,
+  Center,
+}
+
+enum SpawnPosition {
+  Left,
+  Right,
+  Center,
+  Random,
+}
+
+enum GradientMode {
+  Linear,
+  Center,
+}
+
+enum ColorBarMode {
+  LinearHardBorders,
+  LinearSmoothBorder,
+  CenterHardBorders,
+  CenterSmoothBorder,
+}
+
+export enum AnimationType {
   Rainbow,
-  RainbowLinear,
-  RainbowCentered,
-  GradientLinear,
-  GradientCenter,
+  Sparkle,
+  Gradient,
   Static,
-  ColorBarsLinearHard,
-  ColorBarsLinearSoft,
-  ColorBarsCenterHard,
-  ColorBarsCenterSoft,
-  RainbowLinearAccX,
-  RainbowLinearAccY,
-  RainbowCenteredAccX,
-  RainbowCenteredAccY,
-  GradientLinearAccX,
-  GradientLinearAccY,
-  GradientCenteredAccX,
-  GradientCenteredAccY,
-  Custom = 255,
+  ColorBar,
+  RainbowMotion,
+  GradientMotion,
+  FSEQ = 255,
+}
+
+export const AnimationMode = {
+  [AnimationType.Rainbow]: RainbowMode,
+  [AnimationType.Sparkle]: SpawnPosition,
+  [AnimationType.Gradient]: GradientMode,
+  [AnimationType.ColorBar]: ColorBarMode,
+  [AnimationType.RainbowMotion]: RainbowMode,
+  [AnimationType.GradientMotion]: GradientMode,
+} as const;
+
+export enum MotionSensorValue {
+  ACC_X_G = 6,
+  ACC_Y_G = 7,
+  ACC_Z_G = 8,
+  GY_X_DEG = 9,
+  GY_Y_DEG = 10,
+  GY_Z_DEG = 11,
+  PITCH = 12,
+  ROLL = 13,
+  ROLL_COMPENSATED_ACC_X_G = 15,
+  PITCH_COMPENSATED_ACC_Y_G = 16,
 }
---
+++ ui/src/components/Collapsible.tsx
@@ -0,0 +1,53 @@
+import {
+  ArrowsPointingInIcon,
+  ArrowsPointingOutIcon,
+} from '@heroicons/react/24/outline';
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type CollapsibleProps = Omit<
+  CollapsiblePrimitive.CollapsibleProps,
+  'open' | 'onOpenChange'
+>;
+
+export const Collapsible = ({
+  className,
+  defaultOpen = false,
+  ...props
+}: CollapsibleProps): JSX.Element => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(defaultOpen);
+
+  return (
+    <CollapsiblePrimitive.Root
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      {...props}
+    >
+      <div
+        className={twMerge(
+          'group flex w-full select-none items-center justify-between',
+          'focus:outline-none focus-visible:ring focus-visible:ring-sky focus-visible:ring-opacity-75',
+          className,
+        )}
+      >
+        <span className="font-medium underline">{props.title}</span>
+        <CollapsiblePrimitive.Trigger asChild>
+          <button>
+            {isOpen ? (
+              <ArrowsPointingInIcon className="h-6 w-6" />
+            ) : (
+              <ArrowsPointingOutIcon className="h-6 w-6" />
+            )}
+          </button>
+        </CollapsiblePrimitive.Trigger>
+      </div>
+
+      <CollapsiblePrimitive.Content className="mt-4 px-6">
+        {props.children}
+      </CollapsiblePrimitive.Content>
+    </CollapsiblePrimitive.Root>
+  );
+};
+
+export default Collapsible;
