84717316c3c92edb54aeb6afc801a6786c27772e
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2023-01-21 12:57:07 +0100

    feat(ui): add light theme (#115)
    Closes: #71

--- ui/src/components/Switch.tsx
+++ ui/src/components/Switch.tsx
@@ -24,7 +24,7 @@ export const Switch = <T extends FieldValues>({
       className={twMerge(
         'group',
         'radix-state-checked:bg-sky',
-        'radix-state-unchecked:bg-gray',
+        'radix-state-unchecked:bg-zinc dark:radix-state-unchecked:bg-gray',
         'relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out',
         'focus:outline-none focus-visible:ring focus-visible:ring-sky focus-visible:ring-opacity-75',
         className,
@@ -40,7 +40,7 @@ export const Switch = <T extends FieldValues>({
         className={classNames(
           'group-radix-state-checked:translate-x-5',
           'group-radix-state-unchecked:translate-x-0',
-          'pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-neutral shadow-lg ring-0 transition duration-200 ease-in-out',
+          'pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-cloud ring-0 transition duration-200 ease-in-out dark:bg-neutral',
         )}
       />
     </SwitchPrimitive.Root>
---
+++ ui/src/libs/theme.ts
@@ -0,0 +1,11 @@
+export const changeTheme = (theme: string) => {
+  const elThemeColor = document.querySelector('meta[name=theme-color]');
+
+  if (theme === 'light') {
+    document.documentElement.classList.remove('dark');
+    elThemeColor?.setAttribute('content', '#FFFFFF');
+  } else {
+    document.documentElement.classList.add('dark');
+    elThemeColor?.setAttribute('content', '#161717');
+  }
+};
