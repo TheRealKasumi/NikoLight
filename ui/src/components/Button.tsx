eeb14df72445391112c45fb49f9e3fba5c11b060
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2023-03-26 19:12:13 +0200

    feat(motion): add auto motion sensor calibration to ui (#172)

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -1,30 +1,32 @@
+import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type ButtonProps = {
+export type ButtonProps = {
   variant?: 'primary' | 'secondary';
   size?: 'small';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({
-  children,
-  className,
-  variant,
-  size,
-  ...props
-}: ButtonProps) => (
-  <button
-    {...props}
-    className={twMerge(
-      'w-full rounded-md',
-      size === 'small' ? ' py-1' : 'py-2',
-      'disabled:opacity-50',
-      'focus:outline-none focus-visible:ring focus-visible:ring-sky focus-visible:ring-opacity-75',
-      props.type === 'reset' || variant === 'secondary'
-        ? 'border border-gray bg-transparent'
-        : 'bg-sky text-white',
-      className,
-    )}
-  >
-    {children}
-  </button>
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, variant, size, ...props }, forwardedRef) => (
+    <button
+      {...props}
+      ref={forwardedRef}
+      className={twMerge(
+        'w-full rounded-md',
+        size === 'small' ? ' px-2 py-1' : 'px-4 py-2',
+        'disabled:opacity-50',
+        'focus:outline-none focus-visible:ring focus-visible:ring-sky focus-visible:ring-opacity-75',
+        props.type === 'reset' || variant === 'secondary'
+          ? 'border border-gray bg-transparent'
+          : 'bg-sky text-white',
+        className,
+      )}
+    >
+      {children}
+    </button>
+  ),
 );
+
+Button.displayName = 'Button';
+
+export { Button };
